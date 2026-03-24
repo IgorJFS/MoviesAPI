297b01b37599c4378655b776f18ef4b1f5f850fb
IgorJFS <igormpaz@gmail.com>
2026-03-23 22:20:38 -0300

    remove non-used files and update some

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using static System.Net.WebRequestMethods;
+
+namespace MoviesAPI.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+
+public class MovieController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> LerFilmes()
+    {
+        return filmes;
+    }
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
+}
