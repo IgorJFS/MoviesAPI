d7ad39ec64b0b4af0319bde3d40e1ae39cdae2f7
IgorJFS <igormpaz@gmail.com>
2026-03-26 04:23:57 -0300

    add query to the movie

--- MoviesAPI/Controllers/MovieController.cs
+++ MoviesAPI/Controllers/MovieController.cs
@@ -22,13 +22,16 @@ public class MovieController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> LerFilmes()
+    public IEnumerable<Filme> LerFilmes([FromQuery]int skip = 0, 
+        [FromQuery] int take = 5)
     {
-        return filmes;
+        return filmes.Skip(skip).Take(take);
     }
     [HttpGet("{id}")]
-    public Filme? RecuperaFilmePorId(int id)
+    public IActionResult RecuperaFilmePorId(int id)
     {
-        return filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 }
